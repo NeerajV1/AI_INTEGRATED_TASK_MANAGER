0383b4b98badb5723b1cbc44cc172c3424152d8a
NeerajV1 <neerajvazirani@gmail.com>
2026-05-13 21:23:24 +0530

    Deleted older sql file

--- sql/database.sql
+++
@@ -1,9 +0,0 @@
--- Keep the user link (this is safe and good practice)
-ALTER TABLE completed_tasks
-ADD CONSTRAINT fk_completed_user 
-FOREIGN KEY (userid) REFERENCES users(userid);
-
--- DO NOT add the taskid foreign key. 
--- If you already added it, drop it now:
-ALTER TABLE completed_tasks 
-DROP CONSTRAINT completed_tasks_taskid_fkey;
